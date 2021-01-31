a67083b091fb2b874bc3ce04004b0e1d9110ebb4
Tony Tran <codemonkey713@gmail.com>
2021-01-30 23:23:15 -0600

    added questions for the readme and mapped it to the sample readme

---
+++ questions.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    type: 'input',
+    message: 'What is title of the project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Enter the description of your project!',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What are the installation instructions?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Define how to use the application.',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'What is the license for this application?',
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Please provide contributing instructions for this project.',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'What are the testing instructions?',
+    name: 'testing',
+  },
+  {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+];
+
+module.exports = questions;
